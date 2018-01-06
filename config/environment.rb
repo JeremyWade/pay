94a014ff9fa3f21aea7a64c67c9cca66751a5a02
JeremyWade <jeremy.wade.10@gmail.com>
2018-01-06 14:18:44 +0700

    updated config environment with correct domain

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'your_sendgrid_username',
   :password => 'your_sendgrid_password',
-  :domain => 'yourdomain.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
